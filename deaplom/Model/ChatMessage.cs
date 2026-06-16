bb5e5ad79605ca559448fa18bc782eba66ffebb9
Absde168 <154837313+Absde168@users.noreply.github.com>
2026-06-16 23:22:19 +0200

    Fix ChatMessage nullable User navigation property

--- deaplom/Model/ChatMessage.cs
+++ deaplom/Model/ChatMessage.cs
@@ -1,13 +1,17 @@
-﻿namespace deaplom.Model
+using System.Text.Json.Serialization;
+
+namespace deaplom.Model
 {
     public class ChatMessage
     {
         public int Id { get; set; }
         public int UserId { get; set; }
-        public User User { get; set; } = null!;
+
+        [JsonIgnore]
+        public User? User { get; set; }
 
         public string Content { get; set; } = string.Empty;
-        public string SenderType { get; set; } = "User"; 
+        public string SenderType { get; set; } = "User";
         public DateTime Timestamp { get; set; } = DateTime.UtcNow;
     }
 }
